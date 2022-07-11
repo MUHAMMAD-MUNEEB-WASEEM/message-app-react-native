804c6674c1f05f4a575473335a8621cfefbd109c
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-07-11 12:22:44 +0500

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNZRavSRmRLA0uJtJuY3X3dl-V14i0iwI",
+    authDomain: "message-3add3.firebaseapp.com",
+    projectId: "message-3add3",
+    storageBucket: "message-3add3.appspot.com",
+    messagingSenderId: "700486482711",
+    appId: "1:700486482711:web:0a01ab9e76507364e55552"
+  };
