4a60dfff35979b3a47bc400cd4061e192a6c9a52
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-07-11 12:52:35 +0500

    done with login screen

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions={
+  headerStyle: {backgroundColor: "#2C6BED"},
+  headerTitleStyle: {color: "white"},
+  headerTintColor: "white",
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Signal</Text>
-        <StatusBar style="auto" />
-        
-      </View>
+
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen 
+        name="Login"
+        component={LoginScreen}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
